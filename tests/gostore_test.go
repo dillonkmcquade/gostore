56e19eb2662299cee5306f0f1dbd253176eee9ea
Dillon McQuade <dillonkmcquade@gmail.com>
2024-04-11 11:03:57 -0400

    test new conn & grpc error

--- tests/gostore_test.go
+++ tests/gostore_test.go
@@ -2,43 +2,50 @@ package tests
 
 import (
 	"context"
-	"fmt"
 	"testing"
 	"time"
 
 	gostorepb "github.com/dillonkmcquade/gostore/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
 
-func TestNewConn(t *testing.T) {
+func getGRPCClient(t *testing.T) (*grpc.ClientConn, gostorepb.GoStoreClient) {
 	conn, err := grpc.Dial("127.0.0.1:5000", grpc.WithTransportCredentials(insecure.NewCredentials()))
-	defer conn.Close()
 	if err != nil {
 		t.Errorf("Error on net.Dial: %s", err)
 	}
 	c := gostorepb.NewGoStoreClient(conn)
+	return conn, c
+}
+
+func TestNewConn(t *testing.T) {
+	conn, c := getGRPCClient(t)
+	defer conn.Close()
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	r, err := c.Write(ctx, &gostorepb.WriteRequest{Key: "dillon", Payload: "Mcquade"})
+	_, err := c.Read(ctx, &gostorepb.ReadRequest{Key: "dill"})
 	if err != nil {
-		t.Error(err)
+		if _, ok := status.FromError(err); !ok {
+			t.Error("Should respond with grpc error")
+		}
 	}
-	fmt.Println(r.String())
-	if r.GetStatus() != true {
-		t.Error("should be true")
-	}
-	if r.GetMessage() != "Success" {
-		t.Error("should be 'Success'")
-	}
+}
 
-	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
+func TestError(t *testing.T) {
+	conn, client := getGRPCClient(t)
+	defer conn.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	v, _ := c.Read(ctx, &gostorepb.ReadRequest{Key: "dillon"})
-	fmt.Println(v.Value)
-	if v.Value != "Mcquade" {
-		t.Error("should be Mcquade")
+	_, err := client.Read(ctx, &gostorepb.ReadRequest{Key: "notfound"})
+	if s, ok := status.FromError(err); ok {
+		if s.Code() != codes.NotFound {
+			t.Error("Code should be not found")
+		}
 	}
 }
